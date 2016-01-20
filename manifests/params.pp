10cb640138cbade0ab431451dc37da07f74dd5f2
Rory Bramwell <bramwell@pythian.com>
2016-01-20 02:11:11 -0500

    Fixed sytax error with comma

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,10 +24,10 @@ class rundeck_rd_winrm_plugin::params {
       fail("Error: Class['rundeck_rd_winrm_plugin']: Unsupported Operating System: '${::operatingsystem}'")
     }
   }
-  
+
   $rd_winrm_plugin_version = '1.3.2'
   $rundeck_plugins_dir     = '/var/lib/rundeck/libext'
-  $rundeck_user            = 'rundeck',
-  $rundeck_group           = 'rundeck',
+  $rundeck_user            = 'rundeck'
+  $rundeck_group           = 'rundeck'
   $local_tmp_gems_dir      = '/tmp/rundeck/rd-winrm-plugin/gems'
 }
